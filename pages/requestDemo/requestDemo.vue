1242cfeeb4c8390d820887d3a6e9a8072e7e6b68
boomiiing <1215574240@qq.com>
2024-09-06 18:12:16 +0800

    完成

--- pages/requestDemo/requestDemo.vue
+++ pages/requestDemo/requestDemo.vue
@@ -1,5 +1,9 @@
 <template>
 	<view class="contain">
+		<view class="menu">
+			<uni-segmented-control :current="current" :values="items" :style-type="styleType"
+				:active-color="activeColor" @clickItem="onClickItem" />
+		</view>
 		<view class="box" v-for="(item,index) in picList" :key="item._id">
 			<view class="pic">
 				<image lazy-load :src="item.url" mode="widthFix" @click="onPreview(index)"></image>
@@ -9,67 +13,90 @@
 				<view class="author">————{{item.author}}</view>
 			</view>
 		</view>
+		<view class="loadMore">
+			<uni-load-more status="loading"></uni-load-more>
+		</view>
 		<view class="handle">
-			<view class="refresh" @click="onRefresh">刷新</view>
-			<view class="gotop" @click="gotop">顶部</view>
+			<view class="refresh" @click="onRefresh">
+				<uni-icons type="refresh" size="30"> </uni-icons>
+			</view>
+			<view class="gotop" @click="gotop">
+				<uni-icons type="arrow-up" size="30"> </uni-icons>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script setup>
-	import {onReachBottom,onPullDownRefresh} from '@dcloudio/uni-app'
+	import {
+		onReachBottom,
+		onPullDownRefresh
+	} from '@dcloudio/uni-app'
 	const picList = ref([])
+	let current = ref(1)
+	let items = ['所有', '小猫', '小狗']
 	const network = () => {
+		const animalList = ['all', 'cat', 'dog']
 		return uni.request({
 			// url: 'https://api.thecatapi.com/v1/images/search',
 			// url: 'https://tea.qingnian8.com/tools/taoShow',
 			url: 'https://tea.qingnian8.com/tools/petShow',
 			data: {
-				size: 5
+				size: 5,
+				type: animalList[current.value]
 			}
 		})
 	}
 
 	const getCats = () => {
 		uni.showNavigationBarLoading()
-		network().then(res=>{
+		network().then(res => {
 			console.log(res.data)
-			picList.value = [...picList.value,...res.data.data]
+			picList.value = [...picList.value, ...res.data.data]
 			console.log(picList.value)
 			uni.hideNavigationBarLoading()
 		})
 	}
-	const onPreview = (index) =>{
+	const onPreview = (index) => {
 		uni.previewImage({
-			current:index,
-			urls:picList.value.map(ele=>ele.url)
+			current: index,
+			urls: picList.value.map(ele => ele.url)
 		})
 	}
-	const gotop = ()=>{
+	const gotop = () => {
 		uni.pageScrollTo({
-			scrollTop:0,
-			duration:100
+			scrollTop: 0,
+			duration: 100
 		})
 	}
-	const onRefresh = ()=>{
+	const onRefresh = () => {
 		uni.startPullDownRefresh()
 	}
-	onReachBottom(()=>{
-		getCats()
-	})
-	onPullDownRefresh(()=>{
+	const onClickItem = (e) => {
+		console.log(e.currentIndex)
+		current.value = e.currentIndex
 		picList.value = []
 		getCats()
+	}
+	onReachBottom(() => {
+		getCats()
 	})
-	onMounted(()=>{
-			getCats()
-			})
+	onPullDownRefresh(() => {
+		picList.value = []
+		current.value = 0
+		getCats()
+	})
+	onMounted(() => {
+		getCats()
+	})
 </script>
 
 <style lang="scss" scoped>
 	.contain {
-		padding: 50rpx;
-
+		padding:0 50rpx;
+		.menu{
+			margin: 20rpx 0;
+		}
 		.box {
 			margin-bottom: 20rpx;
 			box-shadow: 0 10rpx 40rpx #ccc;
@@ -84,19 +111,27 @@
 
 			.text {
 				padding: 10rpx;
-				.author{
-					color:#ccc;
+
+				.author {
+					color: #ccc;
 					text-align: right;
 				}
 			}
-				
+
 
 		}
-		.handle{
+
+		.loadMore {
+			padding-bottom: calc(env(safe-area-inset-bottom) + 50rpx);
+		}
+
+		.handle {
 			position: fixed;
-			bottom:200rpx;
-			right:20rpx;
-			.refresh,.gotop{
+			bottom: 200rpx;
+			right: 20rpx;
+
+			.refresh,
+			.gotop {
 				width: 90rpx;
 				height: 90rpx;
 				border-radius: 50%;
--- unpackage/dist/dev/mp-weixin/pages/requestDemo/requestDemo.js
+++ unpackage/dist/dev/mp-weixin/pages/requestDemo/requestDemo.js
@@ -1,16 +1,32 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
+if (!Array) {
+  const _easycom_uni_segmented_control2 = common_vendor.resolveComponent("uni-segmented-control");
+  const _easycom_uni_load_more2 = common_vendor.resolveComponent("uni-load-more");
+  const _easycom_uni_icons2 = common_vendor.resolveComponent("uni-icons");
+  (_easycom_uni_segmented_control2 + _easycom_uni_load_more2 + _easycom_uni_icons2)();
+}
+const _easycom_uni_segmented_control = () => "../../uni_modules/uni-segmented-control/components/uni-segmented-control/uni-segmented-control.js";
+const _easycom_uni_load_more = () => "../../uni_modules/uni-load-more/components/uni-load-more/uni-load-more.js";
+const _easycom_uni_icons = () => "../../uni_modules/uni-icons/components/uni-icons/uni-icons.js";
+if (!Math) {
+  (_easycom_uni_segmented_control + _easycom_uni_load_more + _easycom_uni_icons)();
+}
 const _sfc_main = {
   __name: "requestDemo",
   setup(__props) {
     const picList = common_vendor.ref([]);
+    let current = common_vendor.ref(1);
+    let items = ["所有", "小猫", "小狗"];
     const network = () => {
+      const animalList = ["all", "cat", "dog"];
       return common_vendor.index.request({
         // url: 'https://api.thecatapi.com/v1/images/search',
         // url: 'https://tea.qingnian8.com/tools/taoShow',
         url: "https://tea.qingnian8.com/tools/petShow",
         data: {
-          size: 5
+          size: 5,
+          type: animalList[current.value]
         }
       });
     };
@@ -38,11 +54,18 @@ const _sfc_main = {
     const onRefresh = () => {
       common_vendor.index.startPullDownRefresh();
     };
+    const onClickItem = (e) => {
+      console.log(e.currentIndex);
+      current.value = e.currentIndex;
+      picList.value = [];
+      getCats();
+    };
     common_vendor.onReachBottom(() => {
       getCats();
     });
     common_vendor.onPullDownRefresh(() => {
       picList.value = [];
+      current.value = 0;
       getCats();
     });
     common_vendor.onMounted(() => {
@@ -50,7 +73,14 @@ const _sfc_main = {
     });
     return (_ctx, _cache) => {
       return {
-        a: common_vendor.f(common_vendor.unref(picList), (item, index, i0) => {
+        a: common_vendor.o(onClickItem),
+        b: common_vendor.p({
+          current: common_vendor.unref(current),
+          values: common_vendor.unref(items),
+          ["style-type"]: _ctx.styleType,
+          ["active-color"]: _ctx.activeColor
+        }),
+        c: common_vendor.f(common_vendor.unref(picList), (item, index, i0) => {
           return {
             a: item.url,
             b: common_vendor.o(($event) => onPreview(index), item._id),
@@ -59,8 +89,19 @@ const _sfc_main = {
             e: item._id
           };
         }),
-        b: common_vendor.o(onRefresh),
-        c: common_vendor.o(gotop)
+        d: common_vendor.p({
+          status: "loading"
+        }),
+        e: common_vendor.p({
+          type: "refresh",
+          size: "30"
+        }),
+        f: common_vendor.o(onRefresh),
+        g: common_vendor.p({
+          type: "arrow-up",
+          size: "30"
+        }),
+        h: common_vendor.o(gotop)
       };
     };
   }
